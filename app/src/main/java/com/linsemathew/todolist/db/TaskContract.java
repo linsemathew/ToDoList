3655d6570e22ffe2a9278bdf28ce94b3c747126e
Linse Mathew <linsemathew123@gmail.com>
2016-12-09 23:41:10 -0500

    Add TaskDbHelper class.

--- app/src/main/java/com/linsemathew/todolist/db/TaskContract.java
+++ app/src/main/java/com/linsemathew/todolist/db/TaskContract.java
@@ -3,6 +3,7 @@ package com.linsemathew.todolist;
 import android.provider.BaseColumns;
 
 public class TaskContract {
+    //Constants to access database
     public static final String DB_NAME = "com.linsemathew.todolist";
     public static final int DB_VERSION = 1;
 
